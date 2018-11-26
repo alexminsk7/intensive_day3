48f062ecb555309923686f99433762f16f99c97c
Alex <lisouski87@gmail.com>
2018-11-26 19:50:55 +0300

    Второй коммит

--- js/script.js
+++ js/script.js
@@ -2,21 +2,24 @@
 window.addEventListener("DOMContentLoaded", function() {
 	// получаем информацию для работы
 	let products = document.querySelectorAll(".product"),
-			buttons = document.getElementsByTagName("button"),
-			open = document.getElementsByClassName("open")[0];
+	buttons = document.getElementsByTagName("button"),
+	shop = document.getElementsByClassName("shop")[0],
+	open = document.getElementsByClassName("open")[0];
 
 			// Отдельный цикл для кнопки "Закрыть/Close", чтобы не было ошибки с cloneNode
 			for (let i = 0; i < buttons.length; i++) {
 				buttons[i].classList.add("buyButton");
 			}
-			buttons = document.getElementsByClassName("buyButton");
+			
+			buttons = document.querySelectorAll('.buyButton');
+
 	// Функция для создания корзины
 	function createCart() {
 			// Создаем элементы для нашей корзины
 			let cart = document.createElement("div"),
-					field = document.createElement("div"),
-					heading = document.createElement("h2"),
-					close = document.createElement("button");
+			field = document.createElement("div"),
+			heading = document.createElement("h2"),
+			close = document.createElement("button");
 			
 			// Стилизуем элементы с помощью классов
 			cart.classList.add("cart");
@@ -32,26 +35,26 @@ window.addEventListener("DOMContentLoaded", function() {
 			cart.appendChild(heading);
 			cart.appendChild(field);
 			cart.appendChild(close);
-	}
-	createCart();
+		}
+		createCart();
 
-	let field = document.querySelector(".cart-field"),
-			cart = document.querySelector(".cart"),
-			close = document.querySelector(".close");
+		let field = document.querySelector(".cart-field"),
+		cart = document.querySelector(".cart"),
+		close = document.querySelector(".close");
 
 	// Создаем цикл для работы с кнопками
 	for (let i = 0; i < buttons.length; i++) {
+		
 		// Вешаем обработчик событий на каждую кнопку
 		buttons[i].addEventListener("click", function() {
-			
-			// Создаем копию карточки с товаром и перемещаем элементы в корзину
-			let item = products[i].cloneNode(true),
-		      btn = item.querySelector("button");
-      
-      btn.remove();
-      field.appendChild(item);
-      products[i].remove();
-		})
+			if (buttons[i].textContent == "Купить!") {
+				buttons[i].textContent = "Убрать";
+				field.appendChild(products[i]);
+			} else {
+				buttons[i].textContent = "Купить!";
+				shop.appendChild(products[i]);
+			}
+		});
 	}
 
 	// Создаем функцию для открытия и закрытия корзины
